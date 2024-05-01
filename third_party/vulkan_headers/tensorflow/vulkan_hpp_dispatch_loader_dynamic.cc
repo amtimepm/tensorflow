334dee00dba2404ebf06a0392a7698dee2409d51
amtimepm <144833084+amtimepm@users.noreply.github.com>
2024-04-30 19:57:27 -0500

    _vulkan_hpp_dispatch_loader_dynamic.cc

--- third_party/vulkan_headers/tensorflow/vulkan_hpp_dispatch_loader_dynamic.cc
+++ third_party/vulkan_headers/tensorflow/vulkan_hpp_dispatch_loader_dynamic.cc
@@ -17,4 +17,6 @@ limitations under the License.
 
 VULKAN_HPP_DEFAULT_DISPATCH_LOADER_DYNAMIC_STORAGE
 
->@exclude<
+>@exclude< <vulkan/static.hpp>
+
+VULKAN_HPP_STATIC_DISPATCH_LOADER_DYNAMIC_STORAGE
